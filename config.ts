dfbac4c13339d9606b3727249b57b50429a0b7eb
Riley Rohloff <rileyrohloff@workfront.com>
2020-05-18 22:12:38 -0600

    added denon and basic auth for spotify auth

--- config.ts
+++ config.ts
@@ -4,6 +4,6 @@ config({
   export: true,
 });
 
-export const PORT = Deno.env.get('PORT') || 8080;
+export const PORT = 8080 || Deno.env.get("PORT");
 export const clientId = Deno.env.get("CLIENT_ID");
 export const secret = Deno.env.get("CLIENT_SECRET");
